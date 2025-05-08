2415f42c3b0370db9d55961c3a55559358f19419
TcT666346 <3085206942@qq.com>
2025-05-08 15:14:52 +0000

    commit

--- server.py
+++ server.py
@@ -1,3 +1,14 @@
 import socket
 import threading
 import time
+
+# 元组空间，使用字典存储
+tuple_space = {}
+# 操作统计
+total_operations = 0
+read_operations = 0
+get_operations = 0
+put_operations = 0
+error_count = 0
+# 客户端连接总数
+client_count = 0
